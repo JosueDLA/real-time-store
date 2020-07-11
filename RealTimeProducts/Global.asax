296b66cfeb613ef4d403d6c9bbc12fe49b193b0f
Josuedla <josuedlavs@gmail.com>
2020-07-11 03:55:09 -0600

    Agregar archivos de proyecto.

---
+++ RealTimeProducts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RealTimeProducts.MvcApplication" Language="C#" %>
---
+++ RealTimeProducts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RealTimeProducts.Startup))]
+namespace RealTimeProducts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
